278a77c77e1385474a9539166a995d6828c4d151
jray <johnny@johnnyray.me>
2014-12-24 20:20:27 -0500

    no this keyword

--- index.js
+++ index.js
@@ -1,152 +1,154 @@
+
 /* global define: false */
 
 ;(function($){
 
   'use strict';
 
-  var Stream = function( selector ) {
+  var stream = function( selector ) {
 
-    this._domItems = document.querySelectorAll( selector );
-
-    this._events   = [
+    var api       = {};
+    var _domItems = document.querySelectorAll( selector );
+    var _events   = [
       'keyup',
       'keydown',
       'focus',
       'blur',
       'mousemove'
     ];
-    this._buffer   = [ ];
-    this._pipeline = [ ];
-    this._throttle = 10;
-    this._latest   = null;
-    this._event    = null;
-    this._interval = null;
-    this._onEvent  = null;
+    var _buffer   = [ ];
+    var _pipeline = [ ];
+    var _throttle = 10;
+    var _latest   = null;
+    var _event    = null;
+    var _interval = null;
+    var _onEvent  = null;
 
-    return this;
-  };
+    function _performOperation ( item ) {
+      var valid = true;
+      for ( var i in _pipeline ) {
+        var op = _pipeline[ i ];
 
-  Stream.prototype.on = function( event ) {
-    if ( this._events.indexOf( event ) > -1 ) {
-      this._event = event;
-    }
-    return this;
-  };
-
-  Stream.prototype.subscribe = function( onEvent ) {
-    var self = this;
-
-    this._onEvent = function( item ) {
-      var result          = self._performOperation( item );
-      var transformedItem = result.item;
-
-      if ( result.valid ) {
-        self._buffer.push( onEvent.bind( null, transformedItem ) );
-        self._latest = transformedItem;
-      }
-    };
-
-    for ( var i = 0; i < this._domItems.length; i++ ) {
-      this._domItems[ i ].addEventListener( this._event, this._onEvent );
-    }
-    this._execute();
-    return this;
-  };
-
-  Stream.prototype.throttle = function( mills ) {
-    if ( typeof mills === 'number' ) {
-      this._throttle = mills;
-    }
-    this._execute();
-    return this;
-  };
-
-  Stream.prototype.map = function( mapFunc ) {
-    if ( typeof mapFunc === 'function' ) {
-      this._pipeline.push( { map: mapFunc } );
-    }
-    return this;
-  };
-
-  Stream.prototype.pluck = function( pluckOp ) {
-    this._pipeline.push( { pluck: pluckOp } );
-    return this;
-  };
-
-  Stream.prototype.each = function( eachFunc ) {
-    if ( typeof eachFunc === 'function' ) {
-      this._pipeline.push( { each: eachFunc } );
-    }
-    return this;
-  };
-
-  Stream.prototype.filter = function( filterFunc ) {
-    if ( typeof filterFunc === 'function' ) {
-      this._pipeline.push( { filter: filterFunc } );
-    }
-    return this;
-  };
-
-  Stream.prototype.dispose = function() {
-    clearInterval( this._interval );
-    this._el.removeEventListener( this._event, this._onEvent );
-    this._buffer   = [ ];
-    this._pipeline = [ ];
-    this._throttle = 10;
-    this._event    = null;
-    this._interval = null;
-    this._onEvent  = null;
-  };
-
-  // --------------------- private ---------------------
-
-  Stream.prototype._performOperation = function( item ) {
-    var valid = true;
-    for ( var i in this._pipeline ) {
-      var op = this._pipeline[ i ];
-
-      if ( op.map ) {
-        item = op.map( item );
-      } else if ( op.filter ) {
-        if ( !op.filter( item ) ) {
-          valid = false;
-          break;
-        }
-      } else if ( op.each ) {
-        op.each( item );
-      } else if ( op.pluck ) {
-        if ( typeof op.pluck !== 'function' && typeof op.pluck !== 'object' ) {
-          item = item[ op.pluck ] || null;
-        } else if ( typeof op.pluck === 'function' ) {
-          var target = op.pluck( item );
-          if ( item[ target ] ) {
-            item = item[ target ];
-          } else {
+        if ( op.map ) {
+          item = op.map( item );
+        } else if ( op.filter ) {
+          if ( !op.filter( item ) ) {
             valid = false;
+            break;
+          }
+        } else if ( op.each ) {
+          op.each( item );
+        } else if ( op.pluck ) {
+          if ( typeof op.pluck !== 'function' &&
+               typeof op.pluck !== 'object' ) {
+            item = item[ op.pluck ] || null;
+          } else if ( typeof op.pluck === 'function' ) {
+            var target = op.pluck( item );
+            if ( item[ target ] ) {
+              item = item[ target ];
+            } else {
+              valid = false;
+            }
           }
         }
       }
-    }
-    return { valid: valid, item: item };
-  };
+      return { valid: valid, item: item };
+    }  // end _performOperation
 
-  Stream.prototype._execute = function() {
-    var self = this;
+    function _execute() {
 
-    if ( this._interval ) {
-      clearInterval( this._interval );
-    }
-
-    this._interval = setInterval(function(){
-      var response = self._buffer.shift();
-      if ( typeof response === 'function' ) {
-        response();
+      if ( _interval ) {
+        clearInterval( _interval );
       }
-    }, this._throttle);
+
+      _interval = setInterval(function(){
+        var response = _buffer.shift();
+        if ( typeof response === 'function' ) {
+          response();
+        }
+      }, _throttle);
+    }  // end _execute
+
+    api = {
+      on: function( event ) {
+        if ( _events.indexOf( event ) > -1 ) {
+          _event = event;
+        }
+        return api;
+      }, // end on
+
+      subscribe: function( onEvent ) {
+        _onEvent = function( item ) {
+          var result          = _performOperation( item );
+          var transformedItem = result.item;
+
+          if ( result.valid ) {
+            _buffer.push( onEvent.bind( null, transformedItem ) );
+            _latest = transformedItem;
+          }
+        };
+
+        for ( var i = 0; i < _domItems.length; i++ ) {
+          _domItems[ i ].addEventListener( _event, _onEvent, true );
+        }
+        _execute();
+        return api;
+      }, // end subscribe
+
+      throttle: function( mills ) {
+        if ( typeof mills === 'number' ) {
+          _throttle = mills;
+        }
+        _execute();
+        return api;
+      }, // end throttle
+
+      map: function( mapFunc ) {
+        if ( typeof mapFunc === 'function' ) {
+          _pipeline.push( { map: mapFunc } );
+        }
+        return api;
+      }, // end map
+
+      pluck: function( pluckOp ) {
+        _pipeline.push( { pluck: pluckOp } );
+        return api;
+      }, // end pluck
+
+      each: function( eachFunc ) {
+        if ( typeof eachFunc === 'function' ) {
+          _pipeline.push( { each: eachFunc } );
+        }
+        return api;
+      }, // end each
+
+      filter: function( filterFunc ) {
+        if ( typeof filterFunc === 'function' ) {
+          _pipeline.push( { filter: filterFunc } );
+        }
+        return api;
+      }, // end filter
+
+      dispose: function() {
+        clearInterval( _interval );
+        for ( var i = 0; i < _domItems.length; i++ ) {
+          _domItems[ i ].removeEventListener( _event, _onEvent, true );
+        }
+        _buffer   = [ ];
+        _pipeline = [ ];
+        _throttle = 10;
+        _event    = null;
+        _interval = null;
+        _onEvent  = null;
+      }, // end dispose
+
+    }; // end api definition
+
+    return api;
   };
 
   var ValStream = function( selector ) {
-    return new Stream( selector );
+    return stream( selector );
   };
 
   if ( typeof module !== 'undefined' && module.exports ) {
